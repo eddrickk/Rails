99b032581052913fe47238ab08850ee369bfc422
eddrickk <eddricklumine@gmail.com>
2020-04-06 21:34:49 +0700

    First commit

---
+++ blog2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog2::Application.config.secret_token = '59e928bc3ae84aee438fdc6234ccd80fddc949e71bff8e02a0ca6d5c4bef04e0935d7de156512cc7432c7d17ba242fdb420e30db2458184d9fe04d7fbd16f595'
